95b77c0b82652a72e8a2a3a4e5d8ab987264f711
hlixed <onehlixed1@yahoo.com>
2016-03-19 20:39:25 -0400

    Reduce scope to what is necessary; Implementations to follow

--- musiccat.py
+++ musiccat.py
@@ -1,5 +1,5 @@
 # TPPBR MusicCat Song Library v2.3
-# Dependencies: pyyaml, python-Levenshtein, pymongo, pypiwin32
+# Dependencies: pyyaml, python-Levenshtein, pypiwin32
 # Please install all with pip3
 
 # (note: if installing python-Levenshtein complains about vcvarsall.bat,
@@ -14,39 +14,14 @@ except: # Temporary hack until the builtins future module is properly installed
 # pip3 dependencies
 import Levenshtein
 import yaml
-from pymongo import MongoClient
 
 # standard modules
 import os
-import random
-import datetime
 import subprocess
 import logging
-import sys
 from bson import CodecOptions, SON
 
-# TPP modules
-# import tokens, chat
 import winamp
-import selectorcats
-import bidcat
-
-"""
-Expected db formats:
-pbr_ratings:
-   {_id={username, songid}, rating: song rating, last_listened: datetime user last listened}
-pbr_songinfo:
-   {_id=songid, volume_multiplier: volume multiplier}
-"""
-
-
-class BadMatchError(ValueError):
-    """Raised when a song id matches to a song with poor confidence."""
-    def __init__(self, songid, match):
-        super(BadMatchError, self).__init__("Song ID {} not found. Closest match: {}".format(songid, match))
-        self.songid = songid
-        self.match = match
-
 
 class NoMatchError(ValueError):
     """Raised when a song id fails to match a song with any confidence"""
@@ -54,53 +29,16 @@ class NoMatchError(ValueError):
         super(NoMatchError, self).__init__("Song ID {} not found.".format(songid))
         self.songid = songid
 
-
-class InvalidCategoryError(ValueError):
-    """Raised when a category is specified and doesn't match a song"""
-    def __init__(self, songid, category):
-        super(InvalidCategoryError, self).__init__("{} is not a valid category for {}".format(category, songid))
-        self.songid = songid
-        self.category = category
-
-class SongPlayedRecentlyError(ValueError):
-    """Raised when a song cannot be bidded on because it has been played too recently"""
-    def __init__(self, songid, minutes_remaining):
-        super(SongPlayedRecentlyError, self).__init__("That song has been played too recently; try again in {} minutes".format(minutes_remaining))
-        self.songid = songid
-        self.minutes_remaining = minutes_remaining
-
-class SeriesPlayedRecentlyError(ValueError):
-    """Raised when a song cannot be bidded on because songs from the 
-    same series and category have been played too recently"""
-    def __init__(self, songid, series, category, minutes_remaining):
-        super(SeriesPlayedRecentlyError, self).__init__("{}-type songs from the {} series cannot be bidded on for another {} minutes. ".format(category.title(), series, minutes_remaining))
-        self.songid = songid
-        self.series = series
-        self.category = category
-        self.minutes_remaining = minutes_remaining
-
-
-class InsufficientBidError(ValueError):
-    """Raised when an user's bid fails to beat an existing bid"""
-    def __init__(self, bid, current_bid):
-        super(InsufficientBidError, self).__init__("{} does not beat {}".format(bid, current_bid))
-        self.bid = bid
-        self.current_bid = current_bid
-
-
 class MusicCat(object):
 
     def __init__(self, library_path):
-        # Initialize WinAmp and insert addl. function
         self.winamp = winamp.Winamp()
+        self.log = logging.getLogger("musicCat")
+	self.songs = {}
 
-    def play_file(self, songfile):
-        """ Runs Winamp to play given song file"""
-        self.winamp.stop()
-        self.winamp.clearPlaylist()
-        p = subprocess.Popen('"{0}" "{1}"'.format(self.winamp_path, songfile))
+	self.refresh_song_list()
 
-    def load_metadata(self, root_path):
+    def refresh_song_list(self, root_path):
         """ Clears songlist and loads all metadata.yaml files under the root directory"""
         self.songs = {}
         for root, dirs, files in os.walk(root_path):
@@ -111,7 +49,6 @@ class MusicCat(object):
                         self.import_metadata(metafilename)
                     except Exception as e:
                         print("Exception while loading file {}: {}".format(metafilename, e))
-
     """
     Metadata.yaml format:
 
@@ -134,69 +71,46 @@ class MusicCat(object):
         with open(metafilename) as metafile:
             gamedata = yaml.load(metafile)
         path = os.path.dirname(metafilename)
-        newsongs = {}
-
-        if self.song_info:
-            bulkOperation = self.song_info.initialize_unordered_bulk_op() # prepare to update self.song_info
+	newsongs = {}
 
         gameid = gamedata["id"]
         system = gamedata["platform"]
         songs = gamedata.pop("songs")
         for song in songs:
-            if song["id"] in self.songs or song["id"] in newsongs:
-                raise StandardError("Song {} already exists! Not importing {}".format(song["id"], metafilename))
+            if song["id"] in self.songs:
+                self.log.warn("Songid {} exists twice, once in {} and once in {}! Ignoring duplicate. ".format(song["id"], self.songs[song["id"]]["game"]["id"], gameid))
+            if song["id"] in newsongs:
+                self.log.warn("Songid {} exists twice in the same game, {}. Ignoring duplicate.".format(song["id"], gameid))
             song["fullpath"] = os.path.join(path, song["path"])
             song["game"] = gamedata
-            song["lastplayed"] = datetime.datetime.now() - self.time_before_replay
             if "type" in song: # Convert single type to a stored list
                 song["types"] = [song.pop("type")]
-
-            # queue an operation to update self.song_info
-            if self.song_info:
-                bulkOperation.find({'_id': song["id"]}).upsert().update(
-                    {'$setOnInsert': {'volume_multiplier': self.default_song_volume}})
-
             newsongs[song["id"]] = song
-
-        #update series delay database
-        if "series" in gamedata:
-            curr_time = datetime.datetime.now() - self.time_before_replay
-            last_played = {}
-            for category in MusicCat._categories:
-                last_played[category] = curr_time
-
-            self.series_last_played_info.find_one_and_update(
-                {'_id': gamedata["series"]},
-                {'$setOnInsert': last_played},
-                upsert=True)
-
-
-        # do all the updates at once
-        if self.song_info:
-            bulkOperation.execute()
 
         # All data successfully imported; apply to existing metadata
         self.songs.update(newsongs)
 
-    def next_category(self):
-        """Returns the category that follows the currently-playing category"""
-        next_ind = MusicCat._categories.index(self.current_category) + 1
-        if next_ind == len(MusicCat._categories):
-            next_ind = 0
-        return MusicCat._categories[next_ind]
+    def play_file(self, songfile):
+        """ Runs Winamp to play given song file. 
+            Though this may appear to, using subprocess.Popen does not leak memory because winamp makes the processes all exit."""
+        self.winamp.stop()
+        self.winamp.clearPlaylist()
+        p = subprocess.Popen('"{0}" "{1}"'.format(self.winamp_path, songfile))
 
-    def find_song_info(self, songid):
-        """ Fuzzy-match songid to either song id, or full id (game-song)
-
-        Return the song dict of its info, and the songid that matches, in case it was a fuzzy match
-        Raises error on no match.
+    def get_song_info(self, songid):
+        """Return named tuples for a given (valid) songid.
+	Will raise a ValueError if the songid does not exist.
         """
         if songid.find("-") > 0: # Dash separates game and song id
             gameid, songid = songid.split("-")
-        # Try exact match
+        return self.songs[songid]
+
+    def search(self,songid):
+        """Search through all songs in self.songs; return any IDs close to what is typed out."""
+        #Try exact match
         song = self.songs.get(songid, None)
 
-        # Try fuzzy match
+        #If that didn't work, get all songs that seem close enough
         if song is None:
             best_ratio = 0
             best_match = None
@@ -213,200 +127,29 @@ class MusicCat(object):
                 song = self.songs[best_match]
         return song
 
-    def play_next_song(self, category, force_random=False):
-        """ Play a song from the given category: from a bid if one exists, or randomly.
-
-        If a song was queued from a bid, play that (and remove from bid_queue)
-        Otherwise, let the selectorCat pick a song for the given category.
-        Returns the info for the played song, for display status purposes.
-        """
-        
-        if category in self.bid_queue and not force_random:
-            queued = self.bid_queue.pop(category)
-            nextsong = self.songs[queued["song"]]
-            # Charge the user their bid (currently disabled; use bidCat here)
-            #self.bank.adjust_tokens(queued["username"], -queued["tokens"])
-        else:
-            # Otherwise, let the selectorCat decide a random song for this category.
-            try:
-                nextsong = self.selectorcat.get_next_song(category, self.songs)
-            except selectorcats.NoMatchingSongError:
-                # If there isn't a song that matches (such as a game with no warning music) use the default selectorCat
-                # It is assumed that the default selectorcat won't fail to pick a song.
-                nextsong = self.default_selectorcat.get_next_song(category)
-        # Update lastplayed timestamp
-        nextsong["lastplayed"] = datetime.datetime.now()
-        
-        # Update series last played timestamp
-        if "series" in nextsong["game"]:
-            print(nextsong["game"]["series"])
-            self.series_last_played_info.find_one_and_update({"_id": nextsong["game"]["series"]}, 
-                {"$set":{category : datetime.datetime.now()}})
-        
-        # fetch volume and update series last played information
-        if self.song_info:
-            matching_song = self.song_info.find_one({"_id": nextsong["id"]})
-            if matching_song:
-                # assuming that we only want the first match anyways
-                self.current_song_volume = matching_song["volume_multiplier"]
-                self.update_winamp_volume()
-            else:
-                # Volume data should be fed into the database when the metadata files are loaded, but just in case
-                self.log.warn("Volume for Song ID {} not found!".format(nextsong["id"]))
-                self.current_song_volume = default_song_volume
-
-        # And start the song.
-        self.current_category = category
+    def play_next_song(self, songid):
+        """ Play a song. May raise a ValueError if the songid doesn't exist."""
+        nextsong = self.songs[songid]
         self.current_song = nextsong
         self.play_file(nextsong["fullpath"])
         self.log.info("Now playing {}".format(nextsong))
-        return nextsong # Return the song for display purposes
 
-    def bid_command(self, userdata, args):
-        """Handle a bid command
-        userdata: an object; userdata.username is assumed to be a string
-        args: A string: "songid tokens" separated by a space.
-        May throw a ValueError, InvalidCategoryError, or InsufficientBidError (from the self.bid() call).
-        If the song or series has been played recently, may raise a SongPlayedRecentlyError or a SeriesPlayedRecentlyError.
-        """
-        user = userdata.username
-        songid, tokens = args.split(" ")
-        try:
-            tokens = int(tokens)
-        except:
-            raise ValueError("Invalid amount of tokens!")
-            
-        if self.allow_bidding_on_future_categories:
-            self.bid(user, songid, tokens)
-        else: 
-            #only allow bidding on the current category
-            nextcategory = self.next_category()
-            # if you bid during betting (when the next category is technically warning), treat the next category as battle
-            if not self.no_bidding_on_warning and (nextcategory == "warning"):
-                nextcategory = "battle"
-                
-            if nextcategory in songinfo['types']:
-                self.bid(user, songid, tokens, nextcategory)
-            else:
-                raise InvalidCategoryError(nextcategory, songid)
+    def set_winamp_volume(self, volume):
+        """Update winamp's volume. Volume goes from 0 to 1"""
+        if (volume < 0) or (volume > 1):
+            raise ValueError("Volume must be between 0 and 1")
+        #winamp expects a volume from 0 to 255
+        self.winamp.setVolume(volume*255)
 
-    def bid(self, userid, songid, tokens, category=None):
-        """Attempt to place bid to queue song, for a user
+    def pause_winamp(self):
+        pass
 
-        Tokens is assumed validated by the caller.
-        Songid might be invalid; This will attempt a fuzzy match and raise BadMatchError or NoMatchError.
-        May also raise a SongPlayedRecentlyError or a SeriesPlayedRecentlyError.
-        """
-        song = self.find_song_info(songid)
+    def unpause_winamp(self):
+        pass
 
-        if category is None:  # Default to the first type in the song's list.
-            category = song["types"][0]
-            
-        if not self.no_bidding_on_warning and (category == "warning"):
-                category = "battle"
-            
-        if category not in song["types"]:
-            raise InvalidCategoryError(songid, category)
-        
-        #Ensure the song hasn't been played recently
-        if song["lastplayed"] >= datetime.datetime.now() - self.time_before_replay:
-            minutes_remaining = ((song["lastplayed"] - (datetime.datetime.now() - self.time_before_replay)).seconds)/60
-            raise SongPlayedRecentlyError(songid, int(minutes_remaining))
-
-        #If a song from the same series has been played recently, disallow it
-        if self.series_delay_enabled and "series" in song["game"]:
-            series = song["game"]["series"]
-            last_played = self.series_last_played_info.find_one({"_id": series})
-            if last_played:
-               if last_played[category] >= datetime.datetime.now() - self.time_before_series_replay:
-                    minutes_remaining = ((last_played[category] - (datetime.datetime.now() - self.time_before_replay)).seconds )/60
-                    raise SeriesPlayedRecentlyError(songid, series, category, int(minutes_remaining))
-        
-        elif "series" not in song["game"]:
-            self.log.warn("The game {} doesn't have a series tag!".format(song["game"]))
-
-
-        #bidding logic; todo: replace with bidCat
-        current_bid = self.bid_queue.get(category, None)
-        if current_bid is None:  # autowin!
-            self.bid_queue[category] = {"username": userid, "song": song["id"], "tokens": tokens}
-            self.log.info("{} placed bid of {} for {}".format(userid, tokens, song["id"]))
-        elif userid == current_bid["username"]:
-            raise ValueError("Same bidder can't outbid themselves")
-        elif tokens <= current_bid["tokens"]:
-            raise InsufficientBidError(bid, current_bid["tokens"])
-        else:
-            self.bid_queue[category] = {"username": userid, "song": song["id"], "tokens": tokens}
-            self.log.info("Current winner: {} placed bid of {} for {}".format(userid, tokens, song["id"]))
-
-    def rate_command(self, user, args):
-        """Store an user's rating in the database, parsing the command.
-        user: a string
-        args: A string: "<songid> <rating>" separated by a space. """
-        songid, rating = args.split(" ")
-        self.rate(user, songid, rating)    # error checking is all done in here
-
-    def rate(self, user, songid, rating):
-        """ Set a user's rating of a given song"""
-        songid, song = self.find_song_info(songid)
-        if type(rating) != int or rating < 0 or rating >= 5:
-            raise ValueError("Rating must be between 0 and 5.")
-        self.song_ratings.find_one_and_update({"username": user.username, "songid": songid}, {"username": user.username, "songid": songid, "rating": rating}, upsert=True)
-        self.log.info("{} rated songid {} with a rating of {}".format(user.username, songid, rating))
-
-    def set_base_volume(self, basevolume):
-        """Set the base volume for winamp"""
-        self.base_volume = basevolume
-        self.update_winamp_volume()
-
-    def set_current_song_volume(self, songid, volume):
-        """Update the database with the volume for the given song."""
-        if (volume < 0.0) or (volume > 2.0):
-            raise ValueError("Volume multiplier must be between 0 and 2.")
-        updatedSong = self.song_info.update({'_id': songid}, {'_id': songid, "volume_multiplier": volume})
-        if updatedSong is None:
-            raise StandardError("Song ID {} not found!".format(songid))
-        elif songid == currentsongid:
-            self.current_song_volume = volume
-            self.update_winamp_volume()
-            self.log.info("Set volume_multiplier for {} to {}".format(songid, volume))
-
-    def change_selectorcat(self,new_selectorcat_name):
-        """Change the current selectorcat in use, given a string of a class name.
-    Use this for live reloading, so "_changeselector Catamari" could call this.
-    May raise a KeyError if the selectorcat name is invalid or nonexistent.
-    """
-        try:
-            newclass = getattr(selectorcats,new_selectorcat_name)
-        except AttributeError as e:
-            raise KeyError("No selectorcat named {} found; aborting change".format(new_selectorcat_name)) from e
-        #ensure the found class is actually a SelectorCat
-        if issubclass(newclass,selectorcats.SelectorCat):
-            self.selectorcat = newclass()
-            self.log.info("Changed selectorcat to {}".format(new_selectorcat_name))
-        else:
-            raise KeyError("The given name exists, but isn't a valid selectorcat!")
-
-    def update_winamp_volume(self):
-        """Update winamp's volume from self.base_volume and the song's volume_multiplier"""
-        winamp_volume = int(self.base_volume * self.current_song_volume)
-        winamp_volume = min(max(winamp_volume, 0), 255)  # clamp to 0-255
-        self.winamp.setVolume(winamp_volume)
-        # log("set winamp volume to "+str(winamp_volume))
-
-    def set_cooldown(self, time_in_minutes):
-        self.time_before_replay = datetime.timedelta(minutes=time_in_minutes)
+    def print_total_amt_songs(self, category=None):
+        """Print the total number of songs, either for all songs or for a specific category if one is given."""
+        pass
 
 if __name__ == "__main__":
-    import sys
-
-    # Load config from config file
-    config_filename = "config.yaml"
-    library = MusicCat(config_filename)
-    while True:
-        try:
-            category = input("Enter category: ")
-            fullid = input("Enter ID for a song: ")
-            print(library.find_song_info(fullid))
-        except Exception as e:
-            print(e)
+    pass
