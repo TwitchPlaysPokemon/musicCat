d58a486af6a73d295e025ad3c04691f78bb30273
hlixed <onehlixed1@yahoo.com>
2016-04-19 16:24:46 -0400

    Version 3.3, which supports the ends: field

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 setup(
     name="musiccat",
-    version="3.2",
+    version="3.3",
     packages=["musiccat"],
     install_requires=['pyyaml', 'python-Levenshtein', 'pypiwin32'],
 
